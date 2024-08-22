f4f86da1a037dc584974eb5768fb4c216800fa68
Prem <vermaprem.pm@gmail.com>
2024-08-22 22:00:44 +0530

    Data Types

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -6,18 +6,18 @@ console.log(a);
 var b = "Prem";
 {
     var b = "This";     //* var is the globally scoped. redeclared
-    console.log(b);     //* This
+    console.log(b);     //$ This
 }
-console.log(b);         //* This
+console.log(b);         //$ This
 
 
 //?Replace var with the let
 var c = "Hello";
 {
     let c = "Hlo";      //* let is a Block scoped. non-redeclared
-    console.log(c);     //* Hlo
+    console.log(c);     //$ Hlo
 }
-console.log(c);         //* Hello
+console.log(c);         //$ Hello
 
 const author = "Prem";
 //* let author = "Verma"  It throws an error b/c constant cannot be changed
---
+++ chapter1Practice.js
@@ -0,0 +1,39 @@
+//* Q1) Create a variable of type string ad try to add a numer to it
+let a = "Prem";
+let b = 6;
+let c = a + b
+console.log(c)
+
+
+//* Q2) Use typeof operator to find th datatype of the string in last question 
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)   //$ String
+
+
+//* Q3) Create a constant object in javascript. Can you change it to hold a number later
+const item = {
+    name : "Prem",
+    section : 1,
+    isPrincipal : false
+}
+// item = "hlo"        //$ Give the error
+
+
+//* Q4) Try to add a new key to the const object in problem 3. were you able to do it?
+ item['Friend'] = "Shubham"         //* yes we can
+ item['name'] = "verma"         
+ console.log(item)
+//! We can add(modify) or delete the values in the const (object)
+
+
+//* Q5) Write a js program to Create a Word meaning dictionary of 5 words
+const dic = {
+    appreciate : "to enjoy something or to understand the value of somebody/something",
+    gluten : "a sticky substance that is found in grains that we make into flour, for example wheat",
+    feasible : "possible to do",
+    conscientious : " careful to do something correctly and well",
+    democracy : "a system in which the government of a country is elected by the people"
+}
+console.log(dic.feasible)
+console.log(dic["gluten"])
